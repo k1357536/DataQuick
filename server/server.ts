8cdcdbe286d0adf49eba4a7fa3af24d76d8dd29b
k1357536 <capstone@localhost.localdomain>
2017-09-16 03:02:17 -0400

    Add DB access, add util script to init DB

--- server/server.ts
+++ server/server.ts
@@ -2,13 +2,31 @@ import * as express from 'express';
 import * as morgan from 'morgan';
 import * as path from 'path';
 
-let app = express();
+let credentials = require('../dbCredentials.json');
 
+import { Client, QueryResult } from 'pg';
+
+let app = express();
 app.use(morgan('dev'));
+
+let client = new Client(credentials);
+client.connect().catch(e => {
+  console.log(e);
+});
 
 app.use('/shared/', express.static(path.join(__dirname, '../shared/')));
 app.use('/node_modules/', express.static('./node_modules'));
 app.use('/', express.static(path.join(__dirname, '../client/')));
+
+app.get('/api', (req, res, next) => {
+  client.query("SELECT 'Hello world'").then(r => {
+    res.write(r.rows[0]);
+    res.end();
+  }).catch(e => {
+    res.write(e);
+    res.end();
+  });
+})
 
 app.get('*', (req, res, next) => {
   res.sendFile(path.join(__dirname, '../client/index.html'));
---
+++ utils/initDb.ts
@@ -0,0 +1,36 @@
+import { Client, QueryResult } from 'pg';
+
+let credentials = require('../dbCredentials.json');
+let client = new Client(credentials);
+client.connect().then(() => {
+  console.log("drop schemas");
+  return Promise.all([
+    client.query("DROP SCHEMA IF EXISTS data CASCADE;"),
+    client.query("DROP SCHEMA IF EXISTS metadata CASCADE;")
+  ])
+}).then(qr => {
+  console.log("create schemas");
+  // TODO: potential SQL injection
+  return Promise.all([
+    client.query('CREATE SCHEMA data AUTHORIZATION "' + credentials.user + '";'),
+    client.query('CREATE SCHEMA metadata AUTHORIZATION "' + credentials.user + '";')
+  ])
+}).then(qr => {
+  console.log("create tables");
+  return client.query('CREATE TABLE metadata.lists ('
+    + 'sqlName uuid PRIMARY KEY, '
+    + 'prettyName varchar(20) NOT NULL, '
+    + 'columns jsonb NOT NULL, '
+    + 'UNIQUE(prettyName))');
+}).then(qr => {
+  return client.query('CREATE TABLE metadata.dependencies ('
+    + 'pk uuid REFERENCES metadata.lists(sqlName) ON DELETE RESTRICT, '
+    + 'fk uuid REFERENCES metadata.lists(sqlName) ON DELETE CASCADE, '
+    + 'PRIMARY KEY(pk, fk))');
+}).then(qr => {
+  console.log("done");
+  process.exit()
+}).catch(e => {
+  console.log("error: " + e);
+  process.exit()
+});
