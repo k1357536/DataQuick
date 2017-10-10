d50e4933f375b21165702b41943615530ed43911
Michael Hansal <michael.hansal@gmail.com>
2017-10-09 20:06:12 -0400

    Fix line endings

--- utils/initDb.ts
+++ utils/initDb.ts
@@ -2,35 +2,35 @@ import { Client } from 'pg';
 
 (async () => {
   try {
-  let credentials = require('../dbCredentials.json');
-  let client = new Client(credentials);
-  await client.connect();
+    let credentials = require('../dbCredentials.json');
+    let client = new Client(credentials);
+    await client.connect();
 
-  console.log('drop schemas');
-  await client.query('DROP SCHEMA IF EXISTS data CASCADE;');
-  await client.query('DROP SCHEMA IF EXISTS metadata CASCADE;');
+    console.log('drop schemas');
+    await client.query('DROP SCHEMA IF EXISTS data CASCADE;');
+    await client.query('DROP SCHEMA IF EXISTS metadata CASCADE;');
 
-  console.log('create schemas');
-  // TODO: potential SQL injection
-  await client.query('CREATE SCHEMA data AUTHORIZATION "' + credentials.user + '";');
-  await client.query('CREATE SCHEMA metadata AUTHORIZATION "' + credentials.user + '";');
+    console.log('create schemas');
+    // TODO: potential SQL injection
+    await client.query('CREATE SCHEMA data AUTHORIZATION "' + credentials.user + '";');
+    await client.query('CREATE SCHEMA metadata AUTHORIZATION "' + credentials.user + '";');
 
-  console.log('create table lists');
-  await client.query('CREATE TABLE metadata.lists ('
-    + 'id uuid PRIMARY KEY, '
-    + 'name varchar(20) NOT NULL, '
-    + 'columns jsonb NOT NULL, '
-    + 'UNIQUE(name))');
+    console.log('create table lists');
+    await client.query('CREATE TABLE metadata.lists ('
+      + 'id uuid PRIMARY KEY, '
+      + 'name varchar(20) NOT NULL, '
+      + 'columns jsonb NOT NULL, '
+      + 'UNIQUE(name))');
 
-  console.log('create table dependencies');
-  await client.query('CREATE TABLE metadata.dependencies ('
-    + 'reference uuid REFERENCES metadata.lists(id) ON DELETE CASCADE, '
-    + 'tgt uuid REFERENCES metadata.lists(id) ON DELETE RESTRICT, '
-    + 'PRIMARY KEY(reference, tgt))');
+    console.log('create table dependencies');
+    await client.query('CREATE TABLE metadata.dependencies ('
+      + 'reference uuid REFERENCES metadata.lists(id) ON DELETE CASCADE, '
+      + 'tgt uuid REFERENCES metadata.lists(id) ON DELETE RESTRICT, '
+      + 'PRIMARY KEY(reference, tgt))');
 
-  console.log('done');
-  await client.end();
-  } catch(e) {
+    console.log('done');
+    await client.end();
+  } catch (e) {
     console.log(e);
   }
 })();
