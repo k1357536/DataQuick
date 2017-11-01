a0710882cb18b02a199fc401098e3872e22053ee
Michael Hansal <michael.hansal@gmail.com>
2017-11-01 00:38:16 -0400

    extend setup

--- client/setup/setup.component.ts
+++ client/setup/setup.component.ts
@@ -6,6 +6,18 @@ import { DataService } from '../services/data.service';
 import { ColumnType, StringConstraint, NumberConstraint, DateConstraint } from '../../shared/metadata.model';
 import { Columns, Constraints, Folders } from '../../shared/metadata.utils';
 
+interface TestData {
+  age: number,
+  name: string,
+  date_of_birth: Date,
+  executive: boolean,
+  image: Buffer,
+  salary: number,
+  liter: number,
+  completeness: number,
+  ref: number
+};
+
 @Component({
   templateUrl: './setup.component.html',
 })
@@ -44,31 +56,81 @@ export class SetupComponent implements OnInit {
   async create() {
     this.msg = "";
     try {
-      this.metadataService.addFolder("HR", Folders.getRoot());
+      await this.metadataService.addFolder("Test Folder", Folders.getRoot());
 
-      const HR = (await this.metadataService.getFolders()).find(f => f.name === "HR");
+      const f = (await this.metadataService.getFolders()).find(f => f.name === "Test Folder");
 
-      await this.metadataService.addTable("Employees", HR);
+      await this.metadataService.addTable("Test", f);
 
-      const tbl = (await this.metadataService.getTables()).find(tbl => tbl.name === "Employees");
+      const tbl = (await this.metadataService.getTables()).find(tbl => tbl.name === "Test");
 
-      const name = Columns.create("Name", ColumnType.STRING);
-      const age = Columns.create("Age", ColumnType.INT);
-      const birthday = Columns.create("Date of Birth", ColumnType.DATE);
-      const exec = Columns.create("Executive", ColumnType.BOOL);
-      const comment = Columns.create("Comment", ColumnType.STRING);
+      tbl.columns.push(Columns.create("Age", ColumnType.INT,
+        {
+          notNull: true,
+          unique: false,
+          min: 1,
+          max: 120
+        } as NumberConstraint));
 
-      name.constraint = { notNull: true, unique: true, maxLength: 30, regExp: '^[a-zA-Z0-9 ]*$' } as StringConstraint;
-      age.constraint = { notNull: true, unique: false, min: 1, max: 120 } as NumberConstraint;
-      birthday.constraint = { notNull: false, unique: false, min: new Date(1900, 1, 1), max: new Date(2018, 1, 1) } as DateConstraint;
-      exec.constraint = { notNull: true, unique: false };
-      comment.constraint = { notNull: false, unique: false };
+      tbl.columns.push(Columns.create("Name", ColumnType.STRING,
+        {
+          notNull: true,
+          unique: true,
+          maxLength: 30,
+          regExp: '^[a-zA-Z0-9 ]*$'
+        } as StringConstraint
+      ));
 
-      tbl.columns.push(name);
-      tbl.columns.push(age);
-      tbl.columns.push(birthday);
-      tbl.columns.push(exec);
-      tbl.columns.push(comment);
+      tbl.columns.push(Columns.create("Date of Birth", ColumnType.DATE,
+        {
+          notNull: false,
+          unique: false,
+          min: new Date(1900, 1, 1),
+          max: new Date(2018, 1, 1)
+        } as DateConstraint));
+
+      tbl.columns.push(Columns.create("Executive", ColumnType.BOOL,
+        {
+          notNull: true,
+          unique: false
+        }));
+
+      tbl.columns.push(Columns.create("Image", ColumnType.IMAGE,
+        {
+          notNull: false,
+          unique: false
+        }));
+
+      tbl.columns.push(Columns.create("Salary", ColumnType.MONEY,
+        {
+          notNull: true,
+          unique: false,
+          min: 100,
+          max: 1000000
+        } as NumberConstraint));
+
+      tbl.columns.push(Columns.create("Liter", ColumnType.REAL,
+        {
+          notNull: true,
+          unique: false,
+          min: 0.5,
+          max: 3.5
+        } as NumberConstraint));
+
+      tbl.columns.push(Columns.create("Completeness", ColumnType.PERCENT,
+        {
+          notNull: true,
+          unique: false,
+          min: 0,
+          max: 2
+        } as NumberConstraint));
+
+      tbl.columns.push(Columns.create("Ref", ColumnType.FK,
+        {
+          notNull: true,
+          unique: false
+        }));
+
       await this.metadataService.updateTable(tbl);
 
       this.msg = "Create successful!";
@@ -80,18 +142,20 @@ export class SetupComponent implements OnInit {
   async insert() {
     this.msg = "";
     try {
-      const tbl = (await this.metadataService.getTables()).filter(tbl => tbl.name === "Employees")[0];
+      const tbl = (await this.metadataService.getTables()).find(tbl => tbl.name === "Test");
 
-      const data = {
-        name: '',
-        age: 0,
-        date_of_birth: new Date(),
-        executive: true
-      };
       for (let i = 0; i < 20; i++) {
-        data.name = 'Michael ' + String.fromCharCode(20 * Math.random() + 'a'.charCodeAt(0)) + i;
-        data.age = Math.floor(50 * Math.random()) + 20;
-        data.executive = !data.executive;
+        const data: TestData = {
+          name: 'Michael ' + String.fromCharCode(20 * Math.random() + 'a'.charCodeAt(0)) + i,
+          age: Math.floor(50 * Math.random()) + 20,
+          date_of_birth: new Date(),
+          executive: Math.random() > 0.3,
+          image: null,
+          salary: Math.random() * 5000 + 1000,
+          liter: Math.random() * 2 + 1,
+          completeness: Math.random(),
+          ref: Math.min(Math.floor(Math.random() * (i + 1)) + 1, i + 1)
+        };
         await this.dataService.add(tbl.id, data);
       }
 
