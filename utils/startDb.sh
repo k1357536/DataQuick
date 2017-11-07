a049aa15b68e11548e456122c20f5f438012e0a7
Michael Hansal <michael.hansal@gmail.com>
2017-11-07 16:27:38 -0500

    change start and stop scripts

--- utils/startDb.sh
+++ utils/startDb.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
-fi
+[ `whoami` = root ] || exec sudo $0
 
 mkdir -p /var/run/postgresql/ -m 777
 
 su postgres -l -c "/usr/lib/postgresql/9.5/bin/pg_ctl start -D /var/lib/postgresql/DataQuick/"
-
--- utils/stopDb.sh
+++ utils/stopDb.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
-fi
+[ `whoami` = root ] || exec sudo $0
 
 su postgres -l -c "/usr/lib/postgresql/9.5/bin/pg_ctl stop -D /var/lib/postgresql/DataQuick/"
-
